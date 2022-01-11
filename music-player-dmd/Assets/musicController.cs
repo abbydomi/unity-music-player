8334c8b32362182539a1b7b410264e851e6ef0f2
anjuu <68694729+anjuu64@users.noreply.github.com>
2022-01-11 17:31:47 +0100

    Added semi transparent buttons (placeholders)

---
+++ music-player-dmd/Assets/musicController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayRojuu()
+    {
+        Debug.Log("Pongan rojuu");
+    }
+    public void PlayAlexG()
+    {
+        Debug.Log("Nintendo 64 - Alex G");
+    }
+    public void PlayGnash()
+    {
+        Debug.Log("Papi - Gnash");
+    }
+    public void PlayCIA()
+    {
+        Debug.Log("sad emo boys");
+    }
+    public void PlaySuperweaks()
+    {
+        Debug.Log("It'd be a shame if I drowned");
+    }
+}
