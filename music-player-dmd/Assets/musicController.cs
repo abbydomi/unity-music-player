f5fb55bccbb3f75963a56ab391bf06068af722e7
anjuu <68694729+anjuu64@users.noreply.github.com>
2022-01-14 05:28:57 +0100

    Pause, Play, Stop, Previous song

--- music-player-dmd/Assets/musicController.cs
+++ music-player-dmd/Assets/musicController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Linq;
+using UnityEngine.UI;
 
 
 
@@ -14,15 +15,19 @@ public class musicController : MonoBehaviour
     public Animator[] discAnimators;
     public Animator turntableAnimator;
     public TextMeshProUGUI songname; 
+    public Button playPauseButton;
+    public Sprite playSprite;
+    public Sprite pauseSprite;
 
     [HideInInspector]
     public AudioSource selfsource;
-    
+    private bool isPlaying;    
 
     // Start is called before the first frame update
     void Start()
     {
         selfsource = GetComponent<AudioSource>();
+        isPlaying = false;
     }
 
     // Update is called once per frame
@@ -62,6 +67,8 @@ public class musicController : MonoBehaviour
     }
     public void PlaySong(int s)
     {
+        reanimate(); //Reset animators speeds to 1 in case the disc was paused
+
         if (currentsong != s)
         {
             currentsong = s;
@@ -96,6 +103,108 @@ public class musicController : MonoBehaviour
             history.Add(currentsong);
             selfsource.clip = songs[currentsong];
             selfsource.Play();
+            playPauseButton.image.sprite = pauseSprite;
+            isPlaying = true;
+        }
+    }
+    public void PlayPause()
+    {
+        if (isPlaying)
+        {
+            playPauseButton.image.sprite = playSprite;
+            selfsource.Pause();
+            discAnimators[currentsong].speed = 0;
+            turntableAnimator.speed = 0;
+            isPlaying = false;
+
+        }
+        else
+        {
+            playPauseButton.image.sprite = pauseSprite;
+            selfsource.UnPause();
+            discAnimators[currentsong].speed = 1;
+            turntableAnimator.speed = 1;
+            isPlaying = true;
+        }
+        
+    }
+    public void StopTrack()
+    {
+        if (currentsong != -1)
+        {
+            isPlaying = false;
+            reanimate();
+            discAnimators[currentsong].SetTrigger("stop");
+            turntableAnimator.SetTrigger("pausetrigger");
+            selfsource.Stop();
+            currentsong = -1;
+            history.Add(currentsong);
+        }
+        history.Clear();
+        history.Add(-1);
+
+    }
+    public void PrevSong() //Plays the previous song in the history
+    {
+        if (history.Any())
+        {
+            //reanimate();
+            if (history.Last() != -1)
+            {
+                currentsong = history[history.Count() - 2];
+            }
+            
+            if (currentsong != -1)
+            {
+                discAnimators[history.Last()].SetTrigger("stop");
+                turntableAnimator.SetTrigger("pausetrigger");
+                history.Remove(history.Last());
+                selfsource.clip = songs[currentsong];
+                switch (currentsong)
+                {
+                    case 0:
+                        discAnimators[currentsong].Play("rojuuplay");
+                        break;
+                    case 1:
+                        discAnimators[currentsong].Play("alexgplay");
+                        break;
+                    case 2:
+                        discAnimators[currentsong].Play("gnashplay");
+                        break;
+                    case 3:
+                        discAnimators[currentsong].Play("ciaplay");
+                        break;
+                    case 4:
+                        discAnimators[currentsong].Play("supweakplay");
+                        break;
+                }
+                selfsource.Play();
+            }
+            else
+            {
+                if (history.Last() != -1)
+                {
+                    discAnimators[history.Last()].SetTrigger("stop");
+                    turntableAnimator.SetTrigger("pausetrigger");
+                    selfsource.Stop();
+                    history.Clear();
+                    history.Add(-1);
+                }
+            }
+        }
+    }
+    private void reanimate()
+    {
+        if (currentsong != -1)
+        {
+            if (discAnimators[currentsong].speed == 0)
+            {
+                discAnimators[currentsong].speed = 1;
+            }
+            if (turntableAnimator.speed == 0)
+            {
+                turntableAnimator.speed = 1;
+            }
         }
     }
 }
